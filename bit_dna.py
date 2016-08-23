374af472f1cd6ce5b5abf503a4f45ca49154708c
pylego <jaglawz@gmail.com>
2016-08-23 22:44:58 +0800

    feat: dna2int

--- bit_dna.py
+++ bit_dna.py
@@ -5,3 +5,13 @@
 # 16-8-23 4:10PM jaglawz@gmail.com
 #
 # Distributed under terms of the MIT License
+
+
+BASE2INT = {'A': 0b00, 'T': 0b01, 'C': 0b10, 'G': 0b11}
+INT2BASE = {0b00: 'A', 0b01: 'T', 0b10: 'C', 0b11: 'G'}
+INT_LEN = 32  #
+
+
+def dna2bit(dna):
+    bits = []
+    pass
